5d7f43c0c51b868de557b483490f69859c2074ab
ramesh <hari.kvana@gmail.com>
2016-09-12 15:01:00 +0530

    programmatically add sms received

--- app/src/main/java/com/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/broadcastreceiver/MainActivity.java
@@ -1,9 +1,32 @@
 package com.broadcastreceiver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        this.registerReceiver(new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Toast.makeText(context, "Yes! SMS received - Broadcast Receiver Register Programmatically.", Toast.LENGTH_LONG).show();
+            }
+        }, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
